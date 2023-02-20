f65c084af6016e52282a1debeafd9abed18a8a73
Adam Taback <122375166+adamrt27@users.noreply.github.com>
2023-02-20 11:10:57 -0500

    Delete Labs directory

--- Labs/Lab8/webparse.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-text = urllib.parse.quote("https://ca.search.yahoo.com/search?p=engineering+science&fr=yfp-t&fp=1&toggle=1&cop=mss&ei=UTF-8")
-
-f = urllib.request.urlopen(text)
-page = f.read().decode("utf-8")
-f.close()
-print(page)
